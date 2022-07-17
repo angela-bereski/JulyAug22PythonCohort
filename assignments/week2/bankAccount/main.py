d8f012b1afe6fc218d8d880a99df3aecc722be92
angela bereski <angelakbereski@gmail.com>
2022-07-17 13:25:33 -0600

    update bankAccount

--- assignments/week2/bankAccount/main.py
+++ assignments/week2/bankAccount/main.py
@@ -1,10 +1,12 @@
 class BankAccount:
     int_rate= 0.01
     balance= 0
+    all_accounts= []
 
     def __init__(self, int_rate, balance):
         self.int_rate= int_rate
         self.balance= balance
+        BankAccount.all_accounts.append(self)
 
     def deposit(self, amount):
         self.balance= self.balance + amount
@@ -15,7 +17,7 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Balance: ${self.balance}")
+        print(f"Interest Rate: {self.int_rate}\nBalance: ${self.balance}")
         return self
 
     def yield_interest(self):
@@ -23,6 +25,11 @@ class BankAccount:
         import math
         print(f"Interest: ${math.ceil(a_float*100)/100}")
         return self
+    
+    @classmethod
+    def account_info(cls):
+        for account in cls.all_accounts:
+            account.display_account_info()
 
 account_1= BankAccount(0.01, 0)
 account_2= BankAccount(0.01, 0)
@@ -31,3 +38,4 @@ account_1.deposit(500).deposit(400).deposit(250).withdraw(300).yield_interest().
 
 account_2.deposit(125).deposit(375).withdraw(55).withdraw(24).withdraw(12).withdraw(44.02).yield_interest().display_account_info()
 
+BankAccount.account_info()
