a677a0cc7db84ed4a7117f7a71db7c1072e6d996
angela bereski <angelakbereski@gmail.com>
2022-07-27 12:17:50 -0600

    playground

--- assignments/week4/playground/server.py
+++ assignments/week4/playground/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,21 +9,17 @@ def hello():
 
 @app.route('/play')
 def index():
-    return render_template("index.html")
-
+    return render_template("index.html", color= "#6dc2ef", num=3)
 
 @app.route('/play/<int:x>')
 def addBox(x):
     for i in range(0,x):
+        return render_template("index.html", color= "#6dc2ef", num=x)
         
-        
-@app.route('/repeat/<int:num>/<word>')
-def repeatWord(num, word):
-    webWord = ''
-    for i in range(0,num):
-        webWord += f"<p>{word}</p>"
-        print(webWord)
-    return webWord
+@app.route('/play/<int:x>/<string:color>')
+def changeColor(x,color):
+    for i in range(0,x):
+        return render_template("index.html", color=color, num=x)
 
 
 if __name__ == '__main__':
