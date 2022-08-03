4dedf6263d908a3c2bf52ecde85268ce06f5f648
angela bereski <angelakbereski@gmail.com>
2022-08-03 15:52:51 -0600

    userCRUD

--- assignments/week5/userCR/users.sql
+++ assignments/week5/userCR/users.sql
@@ -1,2 +1,6 @@
 SELECT * FROM users_schema.users;
 INSERT INTO users (first_name, last_name, email) VALUES ('Angela', 'Bereski', 'angelakbereski@gmail.com');
+DELETE FROM users WHERE id= 4;
+DELETE FROM users WHERE id= 5;
+DELETE FROM users WHERE id= 6;
+DELETE FROM users WHERE id= 3;
---
+++ assignments/week5/userCRUD/flask_app/controllers/users.py
@@ -0,0 +1,63 @@
+from flask_app import app
+from flask import render_template, redirect, request
+
+from flask_app.models.user import User
+
+
+@app.route('/')
+def read():
+    theUsers = User.get_all()
+    print(theUsers)
+    return render_template('index.html', users = theUsers)
+
+@app.route('/new')
+def new():
+    return render_template('addNew.html')
+
+@app.route('/create_new', methods=["POST"])
+def create_new():
+
+    data = {
+        "first_name": request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"]
+    }
+
+    User.save(data)
+    return redirect('/')
+
+@app.route('/user/<int:user_id>/view/')
+def viewUser(user_id):
+    data = {
+        'id': user_id
+    }
+    theUser = User.getOne(data)
+    return render_template('viewUser.html', user=theUser)
+
+
+@app.route('/user/<int:user_id>/edit/')
+def editUser(user_id):
+    data = {
+        'id': user_id
+    }
+    return render_template('editUser.html', user=User.getOne(data), {'form':form})
+
+@app.route('/user/<int:user_id>/update/', methods=['post'])
+def updateUser(user_id):
+    data = {
+        "id": user_id,
+        "first_name": request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"]
+    }
+    User.update(data)
+    print("updated user controller:", data)
+    return redirect(f'/user/{user_id}/view')
+
+@app.route('/user/<int:user_id>/delete/')
+def deleteUser(user_id):
+    data = {
+        'id': user_id
+    }
+    User.delete(data)
+    return redirect('/')
